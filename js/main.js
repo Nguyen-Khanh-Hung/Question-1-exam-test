8510919349eac517c19f3ae14e591370955ac774
Nguyen Khanh Hung <hungnk19406c@st.uel.edu.vn>
2022-09-14 16:08:11 +0700

    resspion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 const btn_show_menu=document.querySelector('.btn-bars')
+const menu=document.querySelector('.nav-list-resonsive')
+
 const overlay=document.querySelector('.overlay')
+console.log(overlay);
+
 function showMenu(){
-    overlay.classList.add('active')
+    overlay.classList.toggle('active')
+    menu.classList.toggle('active')
+}
+function hideOverlay(){
+    showMenu()
 }
